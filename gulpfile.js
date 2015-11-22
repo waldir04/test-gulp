42a7cdf9d7936cc92e2d1b952a9239a43a122e06
Waldir Martinez <waldir04@gmail.com>
2015-11-21 23:08:42 -0500

    add sass source maps

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,9 @@ gulp.task('vendor:build', function () {
 
 gulp.task('styles', function() {
 	gulp.src('app/assets/sass/**/*.scss')
+	.pipe(sourcemaps.init())
 	.pipe(sass().on('error', sass.logError))
+	.pipe(sourcemaps.write())
 	.pipe(gulp.dest('app/assets/css/'))	
 	.pipe(connect.reload());
 });
